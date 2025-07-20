2616a977da34c02909e2c48be9a4b4c4529dd751
Paulo-Camacho <anthropologyparliament@gmail.com>
2025-07-20 13:39:25 -0700

    added buffer kill map

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -11,13 +11,18 @@ map("i", "jk", "<ESC>")
 -- telescope -- 
 map("n", "<leader>.", "<Cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", { desc = "telescope find all files" })
 
+map("n", "<leader>bx", "<Cmd>bwipeout<CR>", { desc = "kill current buffer" })
+
 map("n", "<leader>fm", require("telescope.builtin").marks, { desc = "telescope marks" })
 
 -- macros --
 -- print the working date --
+
 map("n", "<leader>pd", "<cmd>pu=strftime('%c')<cr>", { desc = "Pasting today's date" })
+
 -- print tick box -- 
 map("n", "<leader>pt", "0i- [ ] ", { noremap = true, silent = true, desc = "Insert a TODO tick box" })
+
 -- move entire lines --
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "shift selected line up"}) 
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "shift selected line down"})
