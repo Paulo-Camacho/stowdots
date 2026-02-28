fef30602742bb315f796e2f1a6ed1e2b42f0c7a9
Paulo-Camacho <anthropologyparliament@gmail.com>
2026-02-27 22:16:41 -0800

    Editing statusline inside of nvchad

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,16 +1,15 @@
 -- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-  theme = "hiberbee",
+  theme = "aylin",
 
   -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
+  --   Comment = { italic = true },
+  --   ["@comment"] = { italic = true },
   -- },
 }
 
@@ -29,15 +28,24 @@ M.nvdash = {
     "",
     "",
   },
-
 }
-
-
 
 M.ui = {
+  statusline = {
+    theme = "vscode_colored", 
+    -- separator_style = "round",
+    -- cwd = current working directory
+    order = { "mode", "f", "git", "%=", "lsp_msg", "%=", "lsp", "cwd", "foo" },
+    modules = {
+      foo = function()
+        return "Hello, Paulo!"
+      end,
+      f = "%F"
+    }
+  },
   tabufline = {
-    lazyload = false
-  }
-}
+    lazyload = false,
+  },
 
+}
 return M
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -7,11 +7,7 @@
 --   {
 --     "neovim/nvim-lspconfig",
 --     config = function()
---       require "configs.lspconfig"
---     end,
---   },
---
--- {
+--       require "configs.lspconfig" end, }, {
 --     'MeanderingProgrammer/render-markdown.nvim',
 --     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
 --     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
@@ -30,10 +26,6 @@
 --    		},
 --    	},
 -- }
---
---
---
---
 
 return {
   {
