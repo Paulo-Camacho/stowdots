7d3d9d9b98ecea629389efb2a299cbda2b8e528d
emperor <anthropologyparliament@gmail.com>
2026-08-09 14:03:03 -0700

    Add clean vanilla Emacs variant

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,8 @@
+;;; init.el --- Deliberately empty vanilla Emacs configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; This file intentionally contains no configuration.
+
+;;; Code:
+
+;;; init.el ends here
